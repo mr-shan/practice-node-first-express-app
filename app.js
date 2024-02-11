4160f24ac608d2138abb51109fcdf162cc89fd53
Shantanu <kulkarni.shantanu4@gmail.com>
2024-02-11 12:10:36 +0530

    Added ejs templating engine

--- app.js
+++ app.js
@@ -7,25 +7,31 @@ const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-  console.log(`Incomming request: ${req.url}`);
-  next();
-})
+// sample middleware
+// app.use((req, res, next) => {
+//   console.log(`Incomming request: ${req.url}`);
+//   next();
+// })
 
 app.use(productRoutes)
 
 app.get('', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  // res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('home', { pageTitle: 'Shop Mart - Home', path: '/' })
 })
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', { pageTitle: '404! Not Found.', path: req.path })
 })
 
 app.listen(3000, () => {
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,54 +12,23 @@ const productsList = [
     name: 'Harry Potter Book',
     price: '500',
     category: 'book',
+    img: 'https://icon-library.com/images/product-icon-png/product-icon-png-19.jpg',
   },
   {
     id: 2,
     name: 'Apple iPhone 15 Pro',
     price: '80,000',
     category: 'mobile',
+    img: 'https://icon-library.com/images/product-icon-png/product-icon-png-19.jpg',
   },
 ];
 
 router.get('/products', (req, res) => {
-  res.send(`   
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Shop Mart - Products Home</title>
-      <link rel="stylesheet" href="css/nav.css">
-      <link rel="stylesheet" href="css/all-products.css">
-    </head>
-    <body>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a class="active" href="/products">All Products</a></li>
-          <li style="float: right;"><a href="/add-product">Add New Product</a></li>
-        </ul>
-      </nav>
-      <div class="all-products">
-        <h1>All Products</h1>
-        <ul>
-          ${productsList
-            .map(
-              (product, index) =>
-                '<li style="animation-delay: ' + index * 40 + 'ms">' +
-                '<img src="https://icon-library.com/images/product-icon-png/product-icon-png-19.jpg"/>' +
-                '<div class="header"><span class="title">' +
-                product.name +
-                '</span> <span class="price">&#8377;' +
-                product.price + '</span></div>' +
-                '<button class="btn-add-cart">' + 'Add to Cart</button></li>'
-            )
-            .join('')}
-        </ul>
-      </div>
-    </body>
-    </html>
-  `);
+  res.render('products', {
+    pageTitle: 'Shop Mart - Products Home',
+    path: '/products',
+    products: productsList
+  })
 });
 
 router.post('/products', (req, res) => {
@@ -69,6 +38,7 @@ router.post('/products', (req, res) => {
       id: productsList.length + 1,
       name: body.name.trim(),
       price: body.price.trim(),
+      img: 'https://icon-library.com/images/product-icon-png/product-icon-png-19.jpg',
     });
     res.redirect('/products');
   } else {
@@ -77,7 +47,11 @@ router.post('/products', (req, res) => {
 });
 
 router.get('/add-product', (req, res) => {
-  res.sendFile(path.join(rootPath, 'views', 'add-product.html'));
+  // res.sendFile(path.join(rootPath, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Shop Mart - Add New Product',
+    path: '/add-product'
+  })
 });
 
 module.exports = router;
